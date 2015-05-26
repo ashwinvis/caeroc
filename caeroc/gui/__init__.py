bc4cb343535c9e626970dd8f99a1a698cf6452c6
Ashwin Vishnu <avmo@kth.se>
2015-05-27 00:43:10 +0200

    GUI changes reloaded.

--- caeroc/gui/__init__.py
+++ caeroc/gui/__init__.py
@@ -1,28 +1,27 @@
 import sys
-from PySide import QtCore, QtGui
+try:
+    from PySide.QtCore import Slot
+    from PySide.QtGui import QDialog, QApplication
+    from caeroc.gui.base_pyside import Ui_CalcDialog
+except ImportError:
+    from PyQt5.QtCore import pyqtSlot as Slot
+    from PyQt5.QtWidgets import QDialog, QApplication
+    from caeroc.gui.base_pyqt import Ui_CalcDialog
 
-from caeroc.gui.base import Ui_Calc
+from caeroc.gui.runtime import CalcDialog
 
-
-class Calc_Dialog(QtGui.QDialog):
-    def __init__(self, parent=None):
-        QtGui.QDialog.__init__(self, parent)
-
-        self.ui = Ui_Calc()
-
-        self.ui.setupUi(self)
-
-    @QtCore.Slot(int)
-    def on_inputSpinBox1_valueChanged(self, value):
-        self.ui.outputWidget.setText(str(value + self.ui.inputSpinBox2.value()))
-
-    @QtCore.Slot(int)
-    def on_inputSpinBox2_valueChanged(self, value):
-        self.ui.outputWidget.setText(str(value + self.ui.inputSpinBox1.value()))
+class CalcApp:
+    def __init__(self):
+        try:
+            self.app = QApplication(sys.argv)
+        except RuntimeError:
+            self.app = QApplication.instance() # if QApplication already exists 
+        self.dialog = CalcDialog()
+        
+    def run(self):
+	self.dialog.show()
+	sys.exit(self.app.exec_())
 
 
 if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
-    calculator = Calc_Dialog()
-    calculator.show()
-    sys.exit(app.exec_())
+    CalcApp().run()
