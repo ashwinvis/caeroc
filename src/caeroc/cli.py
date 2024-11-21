7229e8ea6c224a725ed7842db7f0f83da0ba28d2
Ashwin V. Mohanan <dev@fluid.quest>
2024-11-21 10:25:56 +0100

    Move scripts into functions based entry-points

---
+++ src/caeroc/cli.py
@@ -0,0 +1,25 @@
+import sys
+import subprocess
+
+
+def caeroc_app():
+    import caeroc
+
+    try:
+        caeroc.launch()
+    except AttributeError:
+        app = caeroc.gui.CalcApp()
+        app.run()
+
+
+def caeroc_test():
+    try:
+        subprocess.call("caeroc-app", timeout=4)
+    except subprocess.TimeoutExpired:
+        sys.exit(0)
+    else:
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    caeroc_app()
